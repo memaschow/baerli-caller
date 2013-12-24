04f49a506a12521c1d63452cb73b7d9584b2ce4d
Michael <michael@ubuntu.(none)>
2013-12-24 23:25:50 +0100

    predeploy

--- app/controllers/baerli_controller.rb
+++ app/controllers/baerli_controller.rb
@@ -10,8 +10,8 @@ class BaerliController < ApplicationController
 	@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
 	@twilio_client.account.sms.messages.create(
-	:from => "+15077880701",
-	:to => "+4915161003858",
+	:from => "+4915161003858",
+	:to => "+4917664076045",
 	:body => "Hör auf rumzuzicken, mausi. Du bist die allersüßeste Mausi der Welt! :-* Bis ganz bald. Ich liebe dich.  "
 	)
 
@@ -26,9 +26,9 @@ class BaerliController < ApplicationController
 	@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
 	@twilio_client.account.calls.create(
-	:from => "+4972197795995",
+	:from => "+4915161003858",
 	:to => "+4915161003858",
-	:url => "http://twimlets.com/forward?PhoneNumber=%2B4972197795995&"
+	:url => "http://twimlets.com/forward?PhoneNumber=%2B4917664076045&"
 	)
 
   end
@@ -42,8 +42,8 @@ class BaerliController < ApplicationController
 	@twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
  
 	@twilio_client.account.calls.create(
-	:from => "+4972197795995",
-	:to => "+4915161003858",
+	:from => "+4915161003858",
+	:to => "+4917664076045",
 	:url => "http://twimlets.com/message?Message%5B0%5D=I%20love%20you%2C%20darling.%20I%20wish%20you%20a%20great%20night%20and%20awesome%20dream"
 	)
 
